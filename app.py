384ee961afe9bd38c98571ed5dd143ee1e976ea4
Mohammad Zaman Asif <f20221530@goa.bits-pilani.ac.in>
2026-05-28 23:18:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ st.set_page_config(
 # LOAD DATA
 # ---------------------------------------------------
 
-df = pd.read_csv("../data/operaiq_dataset.csv")
-
+df = pd.read_csv("data/operaiq_dataset.csv")
 # ---------------------------------------------------
 # TITLE
 # ---------------------------------------------------
